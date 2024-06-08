b983753ae140a689a3e0e3c7b486b7ab40dc32cc
Sabrina Velikonja <sabrina.velikonja@hotmail.it>
2024-06-08 16:18:30 +0200

    moved NAv component from App to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import logo from '../assets/icons_assets/Logo.svg';
+import Nav from "./Nav";
 
 function Header() {
   return (
   <header>
     <img src={logo} alt="Little Lemon Logo"/>
+    <Nav/>
   </header>
   )
 }
