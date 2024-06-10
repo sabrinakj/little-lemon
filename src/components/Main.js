adbe5bfc2b5ea080d22301a77dcafafe96ffe552
Sabrina Velikonja <sabrina.velikonja@hotmail.it>
2024-06-10 14:46:58 +0200

    first drafting of the main section

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Main.css';
 
 function Main() {
-  return <main>Main text</main>;
+  return (
+    <main>
+      Main text
+    </main>
+  );
 }
 
 export default Main;
