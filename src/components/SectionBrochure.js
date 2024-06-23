ff97ef1493284281d48adcf3c2aeebe4d4b54538
Sabrina Velikonja <sabrina.velikonja@hotmail.it>
2024-06-23 16:24:32 +0200

    height green background, brochure image border radius and space between, buttons font colors

--- src/components/SectionBrochure.js
+++ src/components/SectionBrochure.js
@@ -5,13 +5,13 @@ function SectionBrochure(props) {
   return (
     <>
       <section className="section-brochure">
-        <div>
+        <div className="sec-brochure-first">
           <h2>{props.title}</h2>
           <h3>{props.subTitle}</h3>
           <p>{props.description}</p>
           <button className="btn-sec-brouchure">Reserve a Table</button>
         </div>
-        <div>
+        <div className="sec-brochure-second">
           <img src={resturantFoodImg} alt="" />
         </div>
       </section>
