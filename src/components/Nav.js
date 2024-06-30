b06027c0a92c62ae95528e53ad9a5877d6d04bd8
sabrinakj <sabrina.velikonja@hotmail.it>
2024-06-30 15:54:41 +0200

    in App and in Nav route links, and components for pages folder

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import './Nav.css';
+import React from "react";
+import "./Nav.css";
 
 function Nav() {
   return (
     <nav>
       <ul>
-        <li href="#home">Home</li>
-        <li href="#about">About</li>
-        <li href="#menu">Menu</li>
-        <li href="#reservation">Reservation</li>
-        <li href="#orderonline">Order Online</li>
-        <li href="#login">Login</li>
+        <li href="#home">
+          <Link to="/">Home</Link>
+        </li>
+        <li href="#about">
+          <Link to="/about">About</Link>
+        </li>
+        <li href="#menu">
+          <Link to="/menu">Menu</Link>
+        </li>
+        <li href="#reservation">
+          <Link to="/reservation">Reservation</Link>
+        </li>
+        <li href="#orderonline">
+          <Link to="/orderonline">Order Online</Link>
+        </li>
+        <li href="#login">
+          <Link to="/login">Login</Link>
+        </li>
       </ul>
     </nav>
-  )
+  );
 }
 
 export default Nav;
---
+++ src/pages/OrderOnline.js
@@ -0,0 +1,10 @@
+function OrderOnline() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the About page.</p>
+    </div>
+  );
+}
+
+export default OrderOnline;
