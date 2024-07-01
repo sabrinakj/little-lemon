f225613b7d23cb1d051eaac81c308f4b5fe45176
sabrinakj <sabrina.velikonja@hotmail.it>
2024-07-01 15:21:16 +0200

    react routing working properly

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import Header from "./components/Header"; // Import the Header component
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import "./App.css";
+import { BrowserRouter as Router} from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <Main />
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from '../pages/Home';
-import About from '../pages/About';
-import Menu from '../pages/Menu';
-import Reservation from '../pages/Reservation';
-import OrderOnline from '../pages/OrderOnline';
-import Login from '../pages/Login';
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Menu from "../pages/Menu";
+import Reservation from "../pages/Reservation";
+import OrderOnline from "../pages/OrderOnline";
+import Login from "../pages/Login";
 
 import "./Main.css";
 
 function Main() {
   return (
     <main className="main">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/reservation" element={<Reservation />} />
-          <Route path="/orderonline" element={<OrderOnline />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/reservation" element={<Reservation />} />
+        <Route path="/orderonline" element={<OrderOnline />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </main>
   );
 }
