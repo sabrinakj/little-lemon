b86cb50f55f655baa27603f74492ab021c07701c
Sabrina Velikonja <sabrina.velikonja@hotmail.it>
2024-08-20 12:06:08 +0200

    build the form structure in the booking form component

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,32 +1,83 @@
 import "./BookingForm.css";
 import resturantImg from "../assets/icons_assets/restaurant.jpg";
+import { useState } from "react";
 
 function BookingForm() {
+  // Define a state variable for each field in the form
+  const [date, setdate] = useState("");
+  const [time, SetTime] = useState("");
+  const [guests, SetGuests] = useState("");
+  const [occasion, setOccasion] = useState("");
+
+  // create a stateful array in the component named availableTimes and use this state variable to populate the time select field options
+  const [availableTimes, setAvailableTimes] = useState([
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00",
+    "22:00",
+  ]);
+
   const handleSubmit = (event) => {
-  }
+    event.preventDefault();
+    console.log({ date, time, guests, occasion, availableTimes });
+  };
 
   return (
     <div>
       <img className="booking-img" src={resturantImg} alt="resturant" />
       <form onSubmit={handleSubmit} className="booking-form-style">
         <label htmlFor="res-date">Choose date</label>
-        <input type="date" id="res-date" />
+        <input
+          type="date"
+          value={date}
+          onChange={(e) => setdate(e.target.value)}
+          name="res-date"
+          id="res-date"
+          required
+        />
+
         <label htmlFor="res-time">Choose time</label>
-        <select id="res-time ">
-          <option>17:00</option>
-          <option>18:00</option>
-          <option>19:00</option>
-          <option>20:00</option>
-          <option>21:00</option>
-          <option>22:00</option>
+        <select
+          name="res-time"
+          id="res-time"
+          value={time}
+          onChange={(e) => SetTime(e.target.value)}
+          required
+        >
+          {availableTimes.map((availableTime) => (
+            <option key={availableTime} value={availableTime}>
+              {availableTime}
+            </option>
+          ))}
         </select>
+
         <label htmlFor="guests">Number of guests</label>
-        <input type="number" placeholder="1" min="1" max="10" id="guests" />
+        <input
+          type="number"
+          name="guests"
+          value={guests}
+          onChange={(e) => SetGuests(e.target.value)}
+          placeholder="1"
+          min="1"
+          max="10"
+          id="guests"
+          required
+        />
+
         <label htmlFor="occasion">Occasion</label>
-        <select id="occasion">
+        <select
+          name="occasion"
+          id="occasion"
+          value={occasion}
+          onChange={(e) => setOccasion(e.target.value)}
+          required
+        >
           <option>Birthday</option>
           <option>Anniversary</option>
         </select>
+
         <input type="submit" value="Make Your reservation" />
       </form>
     </div>
