951450e1dbc3f0b4f0a3b5c972958419698bae58
Sabrina Velikonja <sabrina.velikonkja@hotmail.it>
2024-09-04 15:42:19 +0200

    finished about section, and styled the home page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,33 +1,34 @@
 import "./About.css";
 import imageResturantA from "../assets/icons_assets/Mario and Adrian A.jpg";
-import imageResturantB from "../assets/icons_assets/Mario and Adrian b.jpg";
 
 function About() {
   return (
-    <div className="about-page">
-      <div className="about-div-left">
-        <h1>Little Lemon</h1>
-        <h2>Chicago</h2>
-        <p>
-          Adrian and Mario, two passionate chefs, have opened their dream
-          restaurant, The Little Lemon. This charming eatery offers a delightful
-          culinary experience, with a focus on fresh, seasonal ingredients and
-          Mediterranean flavors. From zesty lemon-infused dishes to classic
-          Italian favorites, The Little Lemon promises to tantalize your taste
-          buds and leave you craving more.
-        </p>
-      </div>
-      <div className="about-div-right">
-        <img
-          className="about-img-one"
-          src={imageResturantA}
-          alt="two chefs inside the kitchen"
-        />
-        <img
-          className="about-img-two"
-          src={imageResturantB}
-          alt="two chefs are cooking"
-        />
+    <div className="about-main-div">
+      <div className="about-page">
+        <div className="about-div-left">
+          <h1 className="about-h1">Little Lemon</h1>
+          <h2 className="about-h2">Chicago</h2>
+          <p className="about-p">
+            Adrian and Mario, two passionate chefs, have opened their dream
+            restaurant, The Little Lemon. This charming eatery offers a
+            delightful culinary experience, with a focus on fresh, seasonal
+            ingredients and Mediterranean flavors. From zesty lemon-infused
+            dishes to classic Italian favorites, The Little Lemon promises to
+            tantalize your taste buds and leave you craving more.
+          </p>
+        </div>
+        <div className="about-div-right">
+          <img
+            className="about-img-one"
+            src={imageResturantA}
+            alt="two chefs inside the kitchen"
+          />
+          <img
+            className="about-img-two"
+            src="https://plus.unsplash.com/premium_photo-1677686707294-e5f616c39e0b?q=80&w=3648&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="two chefs are cooking"
+          />
+        </div>
       </div>
     </div>
   );
