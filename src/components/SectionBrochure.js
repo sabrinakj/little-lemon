9b53a2b93a72911a1e9c16ebe75ad141bb9f8256
Sabrina Velikonja <sabrina.velikonja@hotmail.it>
2025-03-13 12:56:12 +0100

    responsiveness ok for section brochure

--- src/components/SectionBrochure.js
+++ src/components/SectionBrochure.js
@@ -7,15 +7,15 @@ function SectionBrochure(props) {
     <>
       <section className="section-brochure">
         <div className="wrapped-section">
-          <div className="sec-brochure-first">
+          <div className="sec-brochure-text">
             <h2 className="title-section-brochure">{props.title}</h2>
-            <h3>{props.subTitle}</h3>
+            <h3 className="h3-section-brochure">{props.subTitle}</h3>
             <p>{props.description}</p>
             <Link to="/reservation" className="custom-link">
               <button className="btn-sec-brouchure">Reserve a Table</button>
             </Link>
           </div>
-          <div className="sec-brochure-second">
+          <div className="sec-brochure-image">
             <img src={resturantFoodImg} alt="" />
           </div>
         </div>
