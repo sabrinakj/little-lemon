0a9dc4d5b97e07b992ce58452510140ce262047a
Sabrina Velikonja <sabrina.velikonja@hotmail.it>
2025-03-18 11:21:15 +0100

    bug fix in nav desktop view

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,28 +8,55 @@ function Nav({ isVertical, isHamburgerClicked, setIsHamburgerClicked }) {
   const showCloseButton = isHamburgerClicked ? "show-close-sidenav-button" : "hide-close-sidenav-button";
   return (
     <nav className={navContainerClass}>
-      <ul className={navClass}>
-        <li className={showCloseButton} onClick={setIsHamburgerClicked}>X</li>
-        <li className="nav-list"></li>
-        <li className="nav-list" onClick={setIsHamburgerClicked}>
-          <Link to="/" className="nav-custom-link">Home</Link>
-        </li>
-        <li className="nav-list" onClick={setIsHamburgerClicked}>
-          <Link to="/about" className="nav-custom-link">About</Link>
-        </li>
-        <li className="nav-list" onClick={setIsHamburgerClicked}>
-          <Link to="/menu" className="nav-custom-link" state={{ isInOrderOline: false }}>Menu</Link>
-        </li>
-        <li className="nav-list" onClick={setIsHamburgerClicked}>
-          <Link to="/reservation" className="nav-custom-link">Reservation</Link>
-        </li>
-        <li className="nav-list" onClick={setIsHamburgerClicked}>
-          <Link to="/menu" className="nav-custom-link" state={{ isInOrderOline: true }}>Order Online</Link>
-        </li>
-        <li className="nav-list" onClick={setIsHamburgerClicked}>
-          <Link to="/login" className="nav-custom-link">Login</Link>
-        </li>
-      </ul>
+
+      <div className="nav-no-mobile">
+       <ul className={navClass}>
+          <li className="nav-list">
+            <Link to="/" className="nav-custom-link">Home</Link>
+          </li>
+          <li className="nav-list">
+            <Link to="/about" className="nav-custom-link">About</Link>
+          </li>
+          <li className="nav-list">
+            <Link to="/menu" className="nav-custom-link" state={{ isInOrderOline: false }}>Menu</Link>
+          </li>
+          <li className="nav-list">
+            <Link to="/reservation" className="nav-custom-link">Reservation</Link>
+          </li>
+          <li className="nav-list">
+            <Link to="/menu" className="nav-custom-link" state={{ isInOrderOline: true }}>Order Online</Link>
+          </li>
+          <li className="nav-list">
+            <Link to="/login" className="nav-custom-link">Login</Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="nav-mobile">
+        <ul className={navClass}>
+          <li className={showCloseButton} onClick={setIsHamburgerClicked}>X</li>
+          <li className="nav-list"></li>
+          <li className="nav-list" onClick={setIsHamburgerClicked}>
+            <Link to="/" className="nav-custom-link">Home</Link>
+          </li>
+          <li className="nav-list" onClick={setIsHamburgerClicked}>
+            <Link to="/about" className="nav-custom-link">About</Link>
+          </li>
+          <li className="nav-list" onClick={setIsHamburgerClicked}>
+            <Link to="/menu" className="nav-custom-link" state={{ isInOrderOline: false }}>Menu</Link>
+          </li>
+          <li className="nav-list" onClick={setIsHamburgerClicked}>
+            <Link to="/reservation" className="nav-custom-link">Reservation</Link>
+          </li>
+          <li className="nav-list" onClick={setIsHamburgerClicked}>
+            <Link to="/menu" className="nav-custom-link" state={{ isInOrderOline: true }}>Order Online</Link>
+          </li>
+          <li className="nav-list" onClick={setIsHamburgerClicked}>
+            <Link to="/login" className="nav-custom-link">Login</Link>
+          </li>
+        </ul>
+      </div>
+
     </nav>
   );
 }
